c96fe7d03e141d77f838ef801890cc4fe9157376
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2022-11-30 01:21:42 +0200

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bndColor: document.body.style.backgroundColor,
+}
 
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+refs.stopBtn.disabled = true;
+
+let timerId = null;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStart() {
+  onBtnSwitch();
+  timerId = setInterval(renderBnd, 1000);
+}
+
+function onStop() {
+  onBtnSwitch();
+  clearInterval(timerId);
+}
+
+function renderBnd() {
+  document.body.style.backgroundColor = getRandomHexColor()
+}
+
+function onBtnSwitch() {
+  refs.startBtn.toggleAttribute('disabled');
+  refs.stopBtn.toggleAttribute('disabled');
+}
